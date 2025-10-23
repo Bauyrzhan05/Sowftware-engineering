df5fa6d93592587ffba2386c277ddf4e77421d45
Bauyrzhan05 <erman.bauyrzhan@mail.ru>
2025-10-23 17:31:48 +0500

    Delete Lab_5/Repository directory

--- Lab_5/Repository/CoursesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Lab_5.Repository;
-
-import com.example.demo.Lab_5.Entity.Courses;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CoursesRepository extends MongoRepository<Courses, String> {
-}
